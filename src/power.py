519d007bab1284cf7dee7d8dd7c4206299529088
Andres Miguel Airabella <tuk@satellogic.com>
2017-11-08 11:36:37 -0300

    Fixed some ortographic errors. New name for serial port

--- src/power.py
+++ src/power.py
@@ -5,21 +5,23 @@ from tools import convert_current_to_string
 
 class Hantek_PPS2116A(object):
     def __init__(self):
+
+        # List all serial ports. 
+
+        # Find where is connected the power supply
+
+        # Init serial
         self.init_serial()
 
-    # Detectar los Serial Ports
-
-    # Probar conexion e imprimir tipo de device
-
-    # guardar en port el nombre del puerto donde se encontro la fuente
 
     def init_serial(self):
-        self.ser = serial.Serial(
+        self.serial_port = serial.Serial(
             port='/dev/ttySLAB0',
             baudrate=9600,
             timeout=1
         )
-        #print ('Port: ' + self.ser.name)       # check which port was really used
+        #print ('Port: ' + self.serial_port.name)       # check which port was really used
+
 
     def set_voltage(self, voltage):
 
@@ -28,13 +30,14 @@ class Hantek_PPS2116A(object):
             quit()
 
         Ch1VoltageSet = 'su' + convert_voltage_to_string(voltage) + str('\r')
-        self.ser.write(bytes(Ch1VoltageSet, 'utf-8'))
+        self.serial_port.write(bytes(Ch1VoltageSet, 'utf-8'))
         time.sleep(.01)
-        set_voltage_status = str(self.ser.readline())
+        set_voltage_status = str(self.serial_port.readline())
 
         if set_voltage_status[2:4] != 'ok':
             print('I couldn\'t set the voltage as you requested')
             quit()
+
         
     def set_current(self, current):
         if current > 5:
@@ -42,65 +45,69 @@ class Hantek_PPS2116A(object):
             quit()
 
         Ch1AmperageSet = 'si' + convert_current_to_string(current) + str('\r')
-        self.ser.write(bytes(Ch1AmperageSet, 'utf-8'))
+        self.serial_port.write(bytes(Ch1AmperageSet, 'utf-8'))
         time.sleep(.01)
 
-        set_current_status = str(self.ser.readline())
+        set_current_status = str(self.serial_port.readline())
 
         if set_current_status[2:4] != 'ok':
             print('I couldn\'t set the current as you requested')
             quit()
 
+
     def set_on(self):
-        self.ser.write(bytes(str('o1' + '\r'), 'utf-8'))
+        self.serial_port.write(bytes(str('o1' + '\r'), 'utf-8'))
         time.sleep(.01)
 
-        set_on_status = str(self.ser.readline())
+        set_on_status = str(self.serial_port.readline())
 
         if set_on_status[2:4] != 'ok':
-            print('I couldn\'t set the the Power Supply ON as you requested')
+            print('I couldn\'t set the Power Supply ON as you requested')
             quit()
+
 
     def set_off(self):
-        self.ser.write(bytes(str('o0' + '\r'), 'utf-8'))
+        self.serial_port.write(bytes(str('o0' + '\r'), 'utf-8'))
         time.sleep(.01)
 
-        set_off_status = str(self.ser.readline())
+        set_off_status = str(self.serial_port.readline())
 
         if set_off_status[2:4] != 'ok':
-            print('I couldn\'t set the the Power Supply OFF as you requested')
+            print('I couldn\'t set the Power Supply OFF as you requested')
             quit()
 
+
     def read_measured_voltage(self):
-        self.ser.write(bytes(str('rv' + '\r'), 'utf-8'))
-        read_measured_voltage_value = self.ser.readline()
+        self.serial_port.write(bytes(str('rv' + '\r'), 'utf-8'))
+        read_measured_voltage_value = self.serial_port.readline()
         
         return int(read_measured_voltage_value)/100
 
 
     def read_measured_current(self):
-        self.ser.write(bytes(str('ra' + '\r'), 'utf-8'))
-        read_measured_current_value = self.ser.readline()
+        self.serial_port.write(bytes(str('ra' + '\r'), 'utf-8'))
+        read_measured_current_value = self.serial_port.readline()
         
         return int(read_measured_current_value)/1000
 
+
     def read_set_voltage(self):
-        self.ser.write(bytes(str('ru' + '\r'), 'utf-8'))
-        read_set_voltage_value = self.ser.readline()
+        self.serial_port.write(bytes(str('ru' + '\r'), 'utf-8'))
+        read_set_voltage_value = self.serial_port.readline()
         
         return int(read_set_voltage_value)/100
 
 
     def read_set_current(self):
-        self.ser.write(bytes(str('ri' + '\r'), 'utf-8'))
-        read_set_current_value = self.ser.readline()
+        self.serial_port.write(bytes(str('ri' + '\r'), 'utf-8'))
+        read_set_current_value = self.serial_port.readline()
         
         return int(read_set_current_value)/1000
 
 
     def read_status(self):
-        self.ser.write(bytes(str('rs' + '\r'), 'utf-8'))
-        ps_status = self.ser.readline()
+        self.serial_port.write(bytes(str('rs' + '\r'), 'utf-8'))
+        ps_status = self.serial_port.readline()
         
         if   str(ps_status)[5] ==  '1':
             return 'CV'
@@ -111,7 +118,7 @@ class Hantek_PPS2116A(object):
 
     def send_command_and_print_response(self, command):
         
-        print('Response: %s' % self.ser.write(bytes(command + str('\r'), 'utf-8')))
+        print('Response: %s' % self.serial_port.write(bytes(command + str('\r'), 'utf-8')))
         time.sleep(.01)
-        print('Readout:  %s' % self.ser.readline())
+        print('Readout:  %s' % self.serial_port.readline())
         time.sleep(.01)
